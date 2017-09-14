d8d0862cdbd8c88c2394186349a42ff5aea8b0ce
avohra <avohra@engagepeople.com>
2017-09-13 21:52:52 -0400

    completed contact us component, updated header and search

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -10,16 +10,23 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 export class SearchComponent implements OnInit {
 
   public searchIcon: string = AppURLRepo.SEARCH_ICON;
+  public cancelCross: string = AppURLRepo.CLOSE;
 
   public searchForm: FormGroup;
+  public isSearchBarVisibleOnHandheldDevice: boolean;
 
   constructor(private fb: FormBuilder) {
     this.searchForm = this.fb.group({
       search: ['']
     });
+    this.isSearchBarVisibleOnHandheldDevice = false;
   }
 
   ngOnInit() {
+  }
+
+  toggleSearch() {
+    this.isSearchBarVisibleOnHandheldDevice = !this.isSearchBarVisibleOnHandheldDevice;
   }
 
 }
