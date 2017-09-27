8a101ea8592a33c5bb66e40092e4fcc06f94d9da
avohra <avohra@engagepeople.com>
2017-09-26 23:42:02 -0400

    submission form added to contact page

--- src/app/contact-us/contact-us.component.ts
+++ src/app/contact-us/contact-us.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AppURLRepo } from '../../utils/app-url-repo';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-us',
@@ -11,9 +12,28 @@ export class ContactUsComponent implements OnInit {
   public print: string  =  AppURLRepo.PRINT;
   public share: string  = AppURLRepo.SHARE;
 
-  constructor() { }
+  private submissionForm: FormGroup;
+
+  constructor(private fb : FormBuilder) {
+    this.submissionForm = this.fb.group({
+      name: [null, Validators.required],
+      email: [null, Validators.required],
+      documentTitle: [null, Validators.required],
+      documentDescription: [null, Validators.required],
+      datePublished: [null, Validators.required],
+      category: [null, Validators.required],
+      sourceType: [null, Validators.required],
+      keywords: [null, Validators.required],
+      uploadFile: [null],
+    })
+  }
+
 
   ngOnInit() {
+  }
+
+  public onFormSubmit() {
+    console.log(this.submissionForm.value);
   }
 
 }
